29994e1e14063b2f42d3340e41eb2451928dbcd2
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-03 00:12:18 +0800

    I change the (cap) variable to video_capture

--- motion_detection.py
+++ motion_detection.py
@@ -8,12 +8,12 @@ import winsound
 import cv2
 import imutils
 
-cap = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-_, start_frame = cap.read()
+_, start_frame = video_capture.read()
 start_frame = imutils.resize(start_frame, width=500)
 start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
 start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
@@ -33,7 +33,7 @@ def beep_alarm():
 
 while True:
 
-    _, frame = cap.read()
+    _, frame = video_capture.read()
     frame = imutils.resize(frame, width=500)
 
     if alarm_mode:
@@ -59,5 +59,5 @@ while True:
         alarm_mode = False
         break
 
-cap.release()
+video_capture.release()
 cv2.destroyAllWindows()
