f3a64947a0af2633895ff33cf580e1d2d0aac612
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-03 00:15:35 +0800

    I removed the single-letter (_) and replaced it with (return_value)

--- motion_detection.py
+++ motion_detection.py
@@ -13,7 +13,7 @@ video_capture = cv2.VideoCapture(0)
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-_, start_frame = video_capture.read()
+return_value, start_frame = video_capture.read()
 start_frame = imutils.resize(start_frame, width=500)
 start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
 start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
@@ -33,7 +33,7 @@ def beep_alarm():
 
 while True:
 
-    _, frame = video_capture.read()
+    return_value, frame = video_capture.read()
     frame = imutils.resize(frame, width=500)
 
     if alarm_mode:
